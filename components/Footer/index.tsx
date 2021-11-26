b16bb6d41a5cdb435b4511ae3df16368861e2830
Jamie Thalacker <hello@jamiethalacker.dev>
2021-11-25 16:39:52 -0800

    :sparkles: Footer component

---
+++ components/Footer/index.tsx
@@ -0,0 +1,25 @@
+import { Twitter, GitHub } from 'react-feather' 
+import "./index.scss"
+
+export const Footer = () => (
+    <>
+        <footer>
+            <span>
+                <p>Copyright © 2021 Innatical</p>
+                <p>Made with ❤️ in California, Minnesota, & Oregon</p>
+            </span>
+            <div>
+                <table>
+                    <td>
+                        <a href="test">Legal</a>
+                    </td>
+                    <td>
+                        <a href="test">Privacy</a>
+                    </td>
+                </table>
+                <a href="test"><GitHub size={24} /></a>
+                <a href="test"><Twitter size={24} /></a>
+            </div>
+        </footer>
+    </>
+)
