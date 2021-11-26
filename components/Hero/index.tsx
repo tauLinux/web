5acc813c3252d4b6d21124f35ea4da6eed09469e
Jamie Thalacker <hello@jamiethalacker.dev>
2021-11-25 16:40:20 -0800

    :sparkles: Hero Component

---
+++ components/Hero/index.tsx
@@ -0,0 +1,14 @@
+import "./index.scss"
+
+export const Hero = () => (
+    <>
+        <section className={"hero"}>
+            <span>
+                <h2>
+                    <p className={"text-tau"}>tau<p className={"text-linux"}>Linux</p></p>
+                </h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris mi nisi, feugiat nec orci vel, malesuada egestas dolor. Fusce vel nulla id libero faucibus viverra in in leo. Duis vehicula mollis arcu vulputate consectetur.</p>
+            </span>
+        </section>
+    </>
+)
