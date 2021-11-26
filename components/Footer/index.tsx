2ef7342d84515a636de5c18dcf5d88732c754f12
Jamie Thalacker <hello@jamiethalacker.dev>
2021-11-25 22:23:49 -0800

    :page_facing_up: Add license (CC-BY-SA-4.0)

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -8,21 +8,30 @@ export const Footer = () => (
                 <p>Copyright © 2021 Innatical</p>
                 <p>Made with ❤️ in California, Minnesota, & Oregon</p>
             </span>
-            <div>
-                <table>
-                    <td>
-                        <a href="test">Legal</a>
-                    </td>
-                    <td>
-                        <a href="test">Privacy</a>
-                    </td>
-                </table>
-                <a href="test">
-                    <GitHub size={24} />
-                </a>
-                <a href="test">
-                    <Twitter size={24} />
-                </a>
+            <div className={'footer-flex'}>
+                <div>
+                    <table>
+                        <td>
+                            <a href="test">Legal</a>
+                        </td>
+                        <td>
+                            <a href="test">Privacy</a>
+                        </td>
+                    </table>
+                    <a href="https://github.com/tausystems">
+                        <GitHub size={24} />
+                    </a>
+                    <a href="https://twitter.com/tauLinux_">
+                        <Twitter size={24} />
+                    </a>
+                </div>
+                <p>
+                    Content is available under{' '}
+                    <a href="/legal/license">
+                        Attribution-Share Alike 4.0 International
+                    </a>{' '}
+                    unless otherwise noted.
+                </p>
             </div>
         </footer>
     </>
