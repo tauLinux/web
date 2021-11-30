6d58dc7ffa338bbd96d554dd8a3fcd95936d7e19
Jamie Thalacker <hello@jamiethalacker.dev>
2021-11-29 20:05:59 -0800

    Temp Typescript Bug Fix

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Footer } from './Footer'
 import { Header } from './Header'
+//@ts-ignore  TypeScript Bug
 import Head from '@innatical/inn.ts/head'
 
 export const Layout = ({
