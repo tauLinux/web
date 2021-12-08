d3792c30738c886ad128f75dd796104797824b53
Jamie Thalacker <hello@jamiethalacker.dev>
2021-12-07 17:47:31 -0800

    Remove MDX, not needed atm

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import inn from '@innatical/inn.ts'
-import mdx from 'vite-plugin-mdx'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), mdx(), inn()],
+    plugins: [react(), inn()],
 })
