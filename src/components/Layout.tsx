68a49a1a2799268ae12bc390c1553ae4e330668d
Jamie Lee <hello@jamiethalacker.dev>
2022-01-29 00:49:51 -0800

    layout. idk. i'm tired af

---
+++ src/components/Layout.tsx
@@ -0,0 +1,81 @@
+import { globalCss } from '@nextui-org/react'
+import Head from 'next/head'
+import * as React from 'react'
+import { Footer } from './Footer'
+import { Header } from './Header'
+
+const GlobalStyle = globalCss({
+    body: {
+        margin: 0,
+        padding: 0,
+    },
+    html: {
+        margin: 0,
+        padding: 0,
+        fontFamily: 'Roboto, sans-serif',
+    },
+    a: {
+        color: 'inherit',
+        textDecoration: 'none',
+    },
+})
+
+export const Layout = ({
+    title,
+    children,
+}: {
+    title: string
+    children: React.ReactFragment
+}) => {
+    // Update Title
+    title = `${title} • tauOS`
+    GlobalStyle()
+    return (
+        <>
+            <Head>
+                <title>{title}</title>
+
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/apple-touch-icon.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="/favicon-32x32.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                    href="/favicon-16x16.png"
+                />
+                <link rel="manifest" href="/site.webmanifest" />
+
+                <meta name={'description'} content={''} />
+                <meta name={'og:title'} content={title} />
+                <meta name={'og:description'} content={''} />
+                <meta name={'og:type'} content={'website'} />
+                <meta name={'og:image'} content={'/assets/images/icon.png'} />
+                <meta name={'twitter:card'} content={'summary'} />
+                <meta name={'twitter:creator'} content={'@tauOS_'} />
+                <meta name={'twitter:description'} content={''} />
+                <meta name={'twitter:title'} content={title} />
+
+                <link
+                    rel="canonical"
+                    href={
+                        typeof window !== 'undefined'
+                            ? `https://${window.location.host}${window.location.pathname}`
+                            : ''
+                    }
+                />
+            </Head>
+            <Header />
+            {children}
+            <Footer />
+        </>
+    )
+}
