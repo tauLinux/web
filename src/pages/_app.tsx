ec9b17cea0a8c8595c4690c327f69d1faa4ebfb6
Jamie Lee <hello@jamiethalacker.dev>
2022-01-29 00:49:37 -0800

    yeet the sytles :)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import { NextUIProvider } from '@nextui-org/react'
 import type { AppProps } from 'next/app'
-import '../styles/globals.css'
 
 function App({ Component, pageProps }: AppProps) {
     return (
