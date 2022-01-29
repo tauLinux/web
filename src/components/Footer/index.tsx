da9378db3a7317fb5be1d81f60a82981a9da8803
Jamie Lee <hello@jamiethalacker.dev>
2022-01-29 00:20:21 -0800

    Add footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,19 @@
+import { Container, Text } from '@nextui-org/react'
+import { StyledFooter } from './style'
+
+export const Footer = () => {
+    return (
+        <StyledFooter>
+            <Container fluid>
+                <Text color="#ffffff">Copyright © 2021 Innatical</Text>
+                <Text color="#ffffff" weight={'bold'}>
+                    Made with{' '}
+                    <span role="img" aria-label="heart">
+                        ❤️
+                    </span>{' '}
+                    in California, Oregon & Minnesota
+                </Text>
+            </Container>
+        </StyledFooter>
+    )
+}
---
+++ src/components/Footer/style.ts
@@ -0,0 +1,7 @@
+import { styled } from '@nextui-org/react'
+
+export const StyledFooter = styled('footer', {
+    backgroundColor: '#111827',
+    padding: '2.5rem 2rem',
+    margin: '0',
+})
