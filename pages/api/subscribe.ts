52be4e723b95b91ab38d1f960a80ecabc1bbd85d
Lleyton Gray <lleyton@innatical.com>
2022-02-08 20:49:01 -0800

    Add newsletter

---
+++ pages/api/subscribe.ts
@@ -0,0 +1,27 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../util/prisma";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "POST") {
+    const { email } = req.body;
+    if (!email) {
+      console.log("ows", email, req.body);
+      res.status(400).json({ success: false, error: "Email is required" });
+      return;
+    }
+
+    await prisma.email.upsert({
+      where: {
+        email,
+      },
+      update: {},
+      create: { email },
+    });
+
+    res.status(200).json({ success: true });
+  } else {
+    res.status(404).send(undefined);
+  }
+};
+
+export default handler;
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,78 +5,119 @@ import {
   Text,
   Button,
   Spacer,
-  Spinner,
+  Modal,
+  Input,
 } from "@nextui-org/react";
-import { useRouter } from "next/router";
 import Head from "next/head";
+import { useCallback, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const Home = () => {
-  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [open, setOpen] = useState(false);
+  const submit = useCallback(async () => {
+    await fetch("/api/subscribe", {
+      method: "POST",
+      body: JSON.stringify({ email }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }, [email]);
 
   return (
-    <Container css={{ padding: 40, minHeight: "100vh" }} display="flex">
-      <Head>
-        <title>tauOS</title>
-      </Head>
-      <Row align="center">
-        <Col>
-          <Text
-            weight="bold"
-            h1
-            css={{
-              textGradient: "45deg, $blue500 -20%, $pink500 50%",
-              lineHeight: "$sm",
-            }}
-            size={80}
-          >
-            Form.
+    <>
+      <Modal
+        closeButton
+        aria-labelledby="modal-title"
+        open={open}
+        onClose={() => setOpen(false)}
+      >
+        <Modal.Header>
+          <Text id="modal-title" size={18}>
+            Subscribe to our newsletter
           </Text>
-          <Text
-            weight="bold"
-            h1
-            css={{
-              textGradient: "45deg, $purple500 -20%, $pink500 100%",
-              lineHeight: "$sm",
-            }}
-            size={80}
-          >
-            Function.
-          </Text>
-          <Text
-            weight="bold"
-            h1
-            css={{
-              textGradient: "45deg, $red500 -20%, $blue500 100%",
-              lineHeight: "$sm",
-            }}
-            size={80}
-          >
-            Elegance.
-          </Text>
-          <Spacer />
-          <Text style={{ marginBottom: "2rem", width: 500 }}>
-            Introducting tauOS, the modern operating system for creators,
-            developers, gamers, and everyone else. Upgrade your experience with
-            the next generation of systems.
-          </Text>
-          <Container gap={0} display="flex" css={{ gap: 10 }}>
-            <Button rounded color="primary">
-              Learn More
-            </Button>
-            <Button
-              rounded
-              color="secondary"
-              onClick={() => router.push("/download")}
+        </Modal.Header>
+        <Modal.Body>
+          <Input
+            clearable
+            bordered
+            fullWidth
+            color="primary"
+            size="lg"
+            placeholder="Email"
+            contentLeft={<FontAwesomeIcon icon={faEnvelope} />}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button auto flat color="error" onClick={() => setOpen(false)}>
+            Close
+          </Button>
+          <Button auto onClick={() => submit()}>
+            Subscribe
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      <Container css={{ padding: 40, minHeight: "100vh" }} display="flex">
+        <Head>
+          <title>tauOS</title>
+        </Head>
+        <Row align="center">
+          <Col>
+            <Text
+              weight="bold"
+              h1
+              css={{
+                textGradient: "45deg, $blue500 -20%, $pink500 50%",
+                lineHeight: "$sm",
+              }}
+              size={80}
             >
-              Install Now
-            </Button>
-          </Container>
-        </Col>
-        <Col>
-          <img src="/hero.png" />
-        </Col>
-      </Row>
-    </Container>
+              Form.
+            </Text>
+            <Text
+              weight="bold"
+              h1
+              css={{
+                textGradient: "45deg, $purple500 -20%, $pink500 100%",
+                lineHeight: "$sm",
+              }}
+              size={80}
+            >
+              Function.
+            </Text>
+            <Text
+              weight="bold"
+              h1
+              css={{
+                textGradient: "45deg, $red500 -20%, $blue500 100%",
+                lineHeight: "$sm",
+              }}
+              size={80}
+            >
+              Elegance.
+            </Text>
+            <Spacer />
+            <Text style={{ marginBottom: "2rem", width: 500 }}>
+              Introducting tauOS, the modern operating system for creators,
+              developers, gamers, and everyone else. Upgrade your experience
+              with the next generation of systems. Coming soon!
+            </Text>
+            <Container gap={0} display="flex" css={{ gap: 10 }}>
+              <Button rounded color="primary" onClick={() => setOpen(true)}>
+                Subscribe for Updates
+              </Button>
+            </Container>
+          </Col>
+          <Col>
+            <img src="/hero.png" />
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 };
 
