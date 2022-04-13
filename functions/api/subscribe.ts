2ae370f1707102dc6cd4ed8c658a74fbcd5edba6
Lleyton Gray <lea@fyralabs.com>
2022-04-13 00:05:38 -0700

    I wanna play with Cloudflare Workers

---
+++ functions/api/subscribe.ts
@@ -0,0 +1,26 @@
+// import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../util/prisma";
+
+export const onRequestPost: PagesFunction = async (e) => {
+  const { email } = await e.request.json<{ email?: string }>();
+  if (!email)
+    return new Response(
+      JSON.stringify({
+        success: false,
+        error: "Email is required",
+      }),
+      {
+        status: 400,
+      }
+    );
+
+  await prisma.email.upsert({
+    where: {
+      email,
+    },
+    update: {},
+    create: { email },
+  });
+
+  return new Response(null, { status: 200 });
+};
---
+++ functions/cloudflare-env.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="@cloudflare/workers-types" />
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
