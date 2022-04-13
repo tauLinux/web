23822e732f02bb33374561661fb01c0cb006c271
lleyton <lea@fyralabs.com>
2022-04-13 08:45:15 +0000

    ??

---
+++ functions/api/index.ts
@@ -0,0 +1,27 @@
+import { PrismaClient } from "../../node_modules/@prisma/client/index.js";
+
+const prisma = new PrismaClient();
+
+export const onRequestPost: PagesFunction = async (e) => {
+  const { email } = await e.request.json<{ email?: string }>();
+  if (!email)
+    return new Response(
+      JSON.stringify({
+        success: false,
+        error: "Email is required",
+      }),
+      {
+        status: 400,
+      }
+    );
+
+  await prisma.email.upsert({
+    where: {
+      email,
+    },
+    update: {},
+    create: { email },
+  });
+
+  return new Response(null, { status: 200 });
+};
